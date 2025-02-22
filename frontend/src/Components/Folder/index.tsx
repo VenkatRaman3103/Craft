11d81b36a40d90cc82a1ae80d34557b75467ff9c
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-02-22 21:13:29 +0530

    updates
    - add folder name field
    - add slug field
    - update slug field to format the input  text

--- frontend/src/Components/Folder/index.tsx
+++ frontend/src/Components/Folder/index.tsx
@@ -1,22 +1,69 @@
 import axios from "axios";
 import { backendUrl } from "../../config";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./indes.scss";
 
 export const Folder = () => {
+    const [endpointValue, setEndpointValue] = useState("");
+    const CHARACTER_LIMIT = 20;
+
     useEffect(() => {
         async function fetchUser() {
             const response = await axios.get(`${backendUrl}/users`);
-
             console.log(response.data);
         }
         fetchUser();
     }, []);
 
+    const handleEndpointChange = (e) => {
+        const newValue = e.target.value.toLowerCase().replace(/\s+/g, "-");
+        if (newValue.length <= CHARACTER_LIMIT) {
+            setEndpointValue(
+                newValue.startsWith("/") ? newValue : "/" + newValue,
+            );
+        } else {
+            console.log(
+                `Character limit exceeded. Maximum ${CHARACTER_LIMIT} characters allowed.`,
+            );
+        }
+    };
+    const formattedDate = new Date().toLocaleString("en-US", {
+        // weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "2-digit",
+        // hour: "2-digit",
+        // minute: "2-digit",
+        // second: "2-digit",
+        hour12: true,
+    });
+
     return (
         <div className="folder-container">
             <div className="folder-wrapper">
-                <div className="folder-info-container"></div>
+                <div className="folder-info-container">
+                    <div className="name-container">
+                        <input
+                            type="text"
+                            placeholder="Folder name"
+                            className="folder-name"
+                        />
+                        <input
+                            type="text"
+                            className="endpoint-name"
+                            value={endpointValue}
+                            onChange={handleEndpointChange}
+                            maxLength={CHARACTER_LIMIT}
+                            placeholder="/slug"
+                            style={{
+                                width: `calc(${Math.max(50, endpointValue.length * 8)}px + 8px)`,
+                            }}
+                        />
+                    </div>
+                    {/* <div className="details"> */}
+                    {/*     <div>Last edit: {formattedDate}</div> */}
+                    {/* </div> */}
+                </div>
             </div>
         </div>
     );
