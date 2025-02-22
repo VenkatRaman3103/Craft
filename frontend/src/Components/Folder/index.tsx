4576fd261ced0e1232708968315804afcfc5525a
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-02-22 22:38:46 +0530

    update

--- frontend/src/Components/Folder/index.tsx
+++ frontend/src/Components/Folder/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { backendUrl } from "../../config";
+import "./indes.scss";
 
 export const Folder = () => {
     const [endpointValue, setEndpointValue] = useState("");
