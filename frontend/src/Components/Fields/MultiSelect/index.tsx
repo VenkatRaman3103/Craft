f1eea796772f17e7be85abdc7c8a91687384fee9
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-02-28 23:11:41 +0530

    multi select field

--- frontend/src/Components/Fields/MultiSelect/index.tsx
+++ frontend/src/Components/Fields/MultiSelect/index.tsx
@@ -1,7 +1,36 @@
-export const MultiSelect = () => {
+import { field, multiSelect } from "@/Types/fields";
+import { FieldWrapper } from "../FieldWrapper";
+import "./index.scss";
+
+export const MultiSelect = ({ data }: { data: field }) => {
+    console.log(data.value, "dataMultiSelect");
+
+    const multiSelectValues: multiSelect[] = Array.isArray(data.value)
+        ? data.value
+        : [];
+
     return (
-        <div>
-            <div>MultiSelect</div>
-        </div>
+        <FieldWrapper data={data}>
+            <div className="multi-select-field-container">
+                <div className="multi-select-field-wrapper">
+                    {multiSelectValues.map((item, ind) => (
+                        <div
+                            key={ind}
+                            className={`multi-select-field ${multiSelectValues.length - 1 === ind ? "last" : ""}`}
+                        >
+                            <label>
+                                <input
+                                    type="checkbox"
+                                    value={item.value}
+                                    readOnly
+                                    checked={item.checked}
+                                />
+                                {item.value}
+                            </label>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </FieldWrapper>
     );
 };
--- frontend/src/Types/fields.ts
+++ frontend/src/Types/fields.ts
@@ -1,8 +1,13 @@
 export type field = {
-    name: string;
-    type: fieldTypes;
-    label: string;
+    name?: string;
+    type?: fieldTypes;
+    label?: string;
+    value: string | multiSelect[];
+};
+
+export type multiSelect = {
     value: string;
+    checked: boolean;
 };
 
 export type fieldTypes = "text" | "select" | "multi-select";
