d7bd05aff16891b543ca9ce17ad96a705d306d5a
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-02-28 19:30:31 +0530

    styling for the blocks

--- frontend/src/Components/Blocks/index.tsx
+++ frontend/src/Components/Blocks/index.tsx
@@ -1,9 +1,12 @@
 import { Fields } from "../Fields/RenderFields";
 import { blockType } from "@/Types/blocks";
+import "./index.scss";
+import { GripVertical } from "lucide-react";
+import { darkFont, lightFont } from "@/Styles/base";
 
 export const Blocks = ({ blocks }: { blocks: blockType[] }) => {
     return (
-        <div>
+        <div className="blocks-container">
             {blocks.map((item, index) => (
                 <Block key={index} block={item} />
             ))}
@@ -13,9 +16,16 @@ export const Blocks = ({ blocks }: { blocks: blockType[] }) => {
 
 export const Block = ({ block }: { block: blockType }) => {
     return (
-        <div>
-            <h3>{block.type}</h3>
-            <Fields fields={block.fields} />
+        <div className="block-container">
+            <GripVertical size={16} color={lightFont} className="drag-handle" />
+            <div className="block-wrapper">
+                <div className="block-header">
+                    <div className="block-type">{block.type}</div>
+                </div>
+                <div className="fields-container">
+                    <Fields fields={block.fields} />
+                </div>
+            </div>
         </div>
     );
 };
