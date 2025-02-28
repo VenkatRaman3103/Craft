795ddb9f6c0ee8e03590b9b40490fda170687239
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-03-01 02:06:33 +0530

    update

--- frontend/src/Pages/Page/index.tsx
+++ frontend/src/Pages/Page/index.tsx
@@ -1,13 +1,15 @@
 import { Blocks } from "@/Components/Blocks";
 import { PageIntro } from "@/Components/PageIntro";
 import { backendUrl } from "@/config";
+import { pageType } from "@/Types/blocks";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import "./index.scss";
 
 export const Page = () => {
     const { page_id } = useParams();
-    const [pageData, setPageData] = useState();
+    const [pageData, setPageData] = useState<pageType>();
 
     useEffect(() => {
         async function getPageData() {
@@ -22,11 +24,18 @@ export const Page = () => {
     return (
         <div>
             <PageIntro data={pageData} />
-            {pageData && (
-                <div>
-                    <Blocks blocks={pageData.blocks} />
+            <div className="page-content-container">
+                <div className="blocks-list-container">
+                    {pageData && (
+                        <div>
+                            <Blocks blocks={pageData.blocks} />
+                        </div>
+                    )}
                 </div>
-            )}
+                {/* <div className="sidebar-container"> */}
+                {/*     <div className="sidebar-wrapper"></div> */}
+                {/* </div> */}
+            </div>
         </div>
     );
 };
--- frontend/src/Types/blocks.ts
+++ frontend/src/Types/blocks.ts
@@ -7,3 +7,12 @@ export type blockType = {
     text: string;
     fields: fieldsType[];
 };
+
+export type pageType = {
+    page_id: string;
+    created_at: string;
+    edited_at: string;
+    title: string;
+    slug: string;
+    blocks: blockType[];
+};
