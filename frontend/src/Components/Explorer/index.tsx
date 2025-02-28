716a43af7cfdcedd78cf9a00f871be2b22abe3bd
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-03-01 03:24:48 +0530

    update

--- frontend/src/Components/Explorer/index.tsx
+++ frontend/src/Components/Explorer/index.tsx
@@ -2,14 +2,25 @@ import * as React from "react";
 import "./index.scss";
 
 export const Explorer = ({ children }: { children: React.ReactNode }) => {
+    const [isIsometric, setIsIsometric] = React.useState(false);
+
+    const toggleIsometric = () => {
+        setIsIsometric(!isIsometric);
+    };
+
     return (
         <div className="explorer-container">
             <div className="explorer-wrapper">
                 <div className="explorer"></div>
-                <div className="content-container">
+                <div
+                    className={`content-container ${isIsometric ? "isometric" : ""}`}
+                >
                     <div className="content-wrapper">{children}</div>
                 </div>
             </div>
+            <button className="isometric-toggle" onClick={toggleIsometric}>
+                {isIsometric ? "Normal View" : "Isometric View"}
+            </button>
         </div>
     );
 };
