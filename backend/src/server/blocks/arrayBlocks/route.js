2d7c03f5d0cc01aad9f8bbcaf24a253c57d537a0
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-04-01 21:42:44 +0530

    added route to read block based on block_id

--- backend/src/server/blocks/arrayBlocks/route.js
+++ backend/src/server/blocks/arrayBlocks/route.js
@@ -1,13 +1,33 @@
 import express from "express";
 import { db } from "../../server.js";
 import { arrayBlocks } from "../../../db/schema/blocks.js";
+import { eq } from "drizzle-orm";
 
 export const arrayBlocksRouter = express.Router();
 
+// READ
 arrayBlocksRouter.get("/arrayblocks", async (req, res) => {
     try {
         const arrayBlocksResponse = await db.select().from(arrayBlocks);
         res.json(arrayBlocksResponse);
+    } catch (error) {
+        const errorMessage = {
+            error: error.message,
+            origin: "backend/arrayBlocks/read/GET",
+        };
+        console.log(errorMessage);
+        res.status(500).json(errorMessage);
+    }
+});
+
+arrayBlocksRouter.get("/arrayblock/:block_id", async (req, res) => {
+    try {
+        const { block_id } = req.params;
+        const arrayBlockResponse = await db
+            .select()
+            .from(arrayBlocks)
+            .where(eq(arrayBlocks.block_id, block_id));
+        res.json(arrayBlockResponse);
     } catch (error) {
         const errorMessage = {
             error: error.message,
