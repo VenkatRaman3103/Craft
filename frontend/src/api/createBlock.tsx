691522a931583f96b57d9946c1018379cca61e08
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-04-11 17:35:04 +0530

    refactor(createBlock): moved the createBlock function into separate file

---
+++ frontend/src/api/createBlock.tsx
@@ -0,0 +1,114 @@
+import { backendUrl } from "@/config";
+import axios from "axios";
+
+// note: create block
+interface Block {
+    blockType: "normal" | "array";
+}
+
+interface Payload {
+    name: string;
+    description: string;
+    scope: string;
+    blocK_type: string;
+    item_type: string;
+}
+
+export const createBlock = async (
+    page_id: string,
+    scope: "page" | "block",
+    blockName: string,
+    parentBlockId: string | null,
+    block: Block,
+    parentBlockType,
+): Promise<void> => {
+    console.log(block, scope, parentBlockType, "createBlock");
+
+    const payload: Payload = {
+        name: blockName,
+        description: "",
+        scope,
+        blocK_type: block.blockType,
+        item_type: block.blockType,
+    };
+
+    try {
+        const blockResponse = await axios.post(
+            `${backendUrl}/${block.blockType}`,
+            payload,
+        );
+
+        console.log(blockResponse, "blockResponse");
+
+        if (!blockResponse.data.block_id) {
+            throw new Error("Block creation failed, no block_id returned.");
+        }
+
+        const reference_id = blockResponse.data.block_id;
+
+        if (scope === "page") {
+            await createPageItem(page_id, reference_id, block.blockType);
+        } else if (scope === "block" && parentBlockId) {
+            await createBlockItem(
+                parentBlockId,
+                reference_id,
+                block.blockType,
+                parentBlockType,
+            );
+        }
+    } catch (error) {
+        console.error("Error creating block:", error);
+    }
+};
+
+export const createPageItem = async (
+    page_id: string,
+    reference_id: string,
+    itemType: string,
+) => {
+    try {
+        await axios.post(`${backendUrl}/page/${page_id}/page_items`, {
+            reference_id,
+            type: itemType,
+        });
+    } catch (error) {
+        console.error("Error creating page item:", error);
+    }
+};
+
+export const createBlockItem = async (
+    parentBlockId: string,
+    reference_id: string,
+    blockType: "normal" | "array",
+    parentBlockType: string,
+) => {
+    console.log("entering into createBlockItem");
+    try {
+        if (blockType === "normal") {
+            console.log(
+                `${backendUrl}/${blockType}/${parentBlockId}/block_items`,
+                "Normal block selected",
+            );
+            await axios.post(
+                `${backendUrl}/${parentBlockType}/${parentBlockId}/block_items`,
+                {
+                    reference_id,
+                    parent_block_id: parentBlockId,
+                    type: blockType,
+                },
+            );
+        } else if (blockType === "array") {
+            console.log("Normal block selected");
+            await axios.post(
+                `${backendUrl}/${parentBlockType}/${parentBlockId}/block_items`,
+                {
+                    reference_id,
+                    parent_block_id: parentBlockId,
+                    type: blockType,
+                },
+            );
+        }
+    } catch (error) {
+        console.error("Error creating block item:", error);
+    }
+};
---
+++ notes/11-april/todo.md
@@ -0,0 +1,29 @@
+# array block
+
+## array block template
+
+### front-end
+
+- use react query for below operations
+
+- [ ] connect front-end with backend for the array blocks
+- [ ] create dummy templates in the backend and render it in the front-end
+- [ ] create actuall templates from the front-end, check backend is updating correctly and render it
+
+#### Duplicatiing functionality for the array_template
+
+- [ ] connect add template button with `array_block_templates`
+- [ ] add a function in the backend to duplicate the content of template
+
+### db
+
+<!-- yet to update -->
+
+### api
+
+<!-- yet to update -->
+
+### nesting functionality
+
+- [ ] creation of `array_template` from the frontend while creating the `array_block`
+- [ ] rendering of each blocks withing each template
