69118c6d27afaeb8b14b3858acfaaecc75e475bb
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-05-18 23:42:28 +0530

    feat(FontsControlPanel): added following UI components
    - font style
    - font width
    - font size
    - font decoration
    - font spacing
    - text align

--- frontend/src/Canvas/index.tsx
+++ frontend/src/Canvas/index.tsx
@@ -4,17 +4,26 @@ import { ElementPicker, iconStrockWidth } from "./ElementPicker";
 import { elementType } from "@/Types/canvas/elementsType";
 import { PublishFeature } from "./PublishFeature";
 import {
+    AlignCenter,
     AlignCenterHorizontal,
     AlignCenterVertical,
     AlignEndHorizontal,
     AlignEndVertical,
+    AlignHorizontalSpaceAround,
+    AlignHorizontalSpaceAroundIcon,
+    AlignLeft,
+    AlignRight,
     AlignStartHorizontal,
     AlignStartVertical,
     AlignVerticalJustifyEnd,
     AlignVerticalJustifyStart,
+    AlignVerticalSpaceAround,
+    AlignVerticalSpaceAroundIcon,
     ArrowDown,
     ArrowLeft,
     ArrowUp,
+    Bold,
+    Italic,
     Minus,
     Monitor,
     Scan,
@@ -25,6 +34,8 @@ import {
     SquareDashedTopSolid,
     SquareRoundCorner,
     Tablet,
+    Type,
+    Underline,
 } from "lucide-react";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import {
@@ -259,8 +270,8 @@ export const Canvas: React.FC = () => {
                             <div className="dimension">0</div>
                         </div>
                     </div>
+                    <FontsControlPanel />
                     <BorderControlPanel />
-
                     <AlignmentControlPanel />
                     <div className="box-model-container toolbar-section">
                         <div className="heading">box model</div>
@@ -268,6 +279,111 @@ export const Canvas: React.FC = () => {
                             <div className="border">
                                 <div className="padding">
                                     <div className="content"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export const FontsControlPanel = () => {
+    const [activeDecoration, setActiveDecoration] = useState("bold");
+    const [activeAlignType, setActiveAlignType] = useState("left-align");
+    const fontIconSize = 16;
+    return (
+        <div className="fonts-section-container toolbar-section">
+            <div className="heading">font</div>
+            <div className="font-sizing-sub-section">
+                <div className="font-sizing-section">
+                    <div className="heading">font</div>
+                    <div className="font-sizing-tools-container">
+                        <select className="font-style-select select-drop-down">
+                            <option>option 1</option>
+                            <option>option 2</option>
+                            <option>option 3</option>
+                        </select>
+                        <select className="font-width-select select-drop-down">
+                            <option>option 1</option>
+                            <option>option 2</option>
+                            <option>option 3</option>
+                        </select>
+                        <div className="font-decoration-tools">
+                            <div className="font-size font-tool">
+                                <Type size={fontIconSize} />
+                                <input type="number" />
+                            </div>
+                            <div className="font-decoration-options">
+                                <div
+                                    className={`font-bold decoration-tool ${activeDecoration == "bold" ? "active" : ""}`}
+                                    onClick={() => setActiveDecoration("bold")}
+                                >
+                                    <Bold size={fontIconSize} />
+                                </div>
+                                <div
+                                    className={`font-italic decoration-tool ${activeDecoration == "italic" ? "active" : ""}`}
+                                    onClick={() =>
+                                        setActiveDecoration("italic")
+                                    }
+                                >
+                                    <Italic size={fontIconSize} />
+                                </div>
+                                <div
+                                    className={`font-underline decoration-tool ${activeDecoration == "underline" ? "active" : ""}`}
+                                    onClick={() =>
+                                        setActiveDecoration("underline")
+                                    }
+                                >
+                                    <Underline size={fontIconSize} />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="font-spacing-tools">
+                            <div className="font-top-bottom-space font-space font-tool">
+                                <AlignVerticalSpaceAroundIcon
+                                    size={fontIconSize}
+                                />
+                                <input type="number" />
+                            </div>
+                            <div className="font-left-bottom-space font-space font-tool">
+                                <AlignHorizontalSpaceAroundIcon
+                                    size={fontIconSize}
+                                />
+                                <input type="number" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="font-align-section">
+                    <div className="heading">align</div>
+                    <div className="font-align-tools-container">
+                        <div className="font-align-tools">
+                            <div className="font-align-decoration-options">
+                                <div
+                                    className={`left-align-tool align-tool ${activeAlignType == "left-align" ? "active" : ""}`}
+                                    onClick={() =>
+                                        setActiveAlignType("left-align")
+                                    }
+                                >
+                                    <AlignLeft size={fontIconSize} />
+                                </div>
+                                <div
+                                    className={`centre-align-tool align-tool ${activeAlignType == "centre-align" ? "active" : ""}`}
+                                    onClick={() =>
+                                        setActiveAlignType("centre-align")
+                                    }
+                                >
+                                    <AlignCenter size={fontIconSize} />
+                                </div>
+                                <div
+                                    className={`right-align-tool align-tool ${activeAlignType == "right-align" ? "active" : ""}`}
+                                    onClick={() =>
+                                        setActiveAlignType("right-align")
+                                    }
+                                >
+                                    <AlignRight size={fontIconSize} />
                                 </div>
                             </div>
                         </div>
