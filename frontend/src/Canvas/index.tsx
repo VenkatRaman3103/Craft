8363102cdf5025f60af0e63b7e34130b7c0f5e9a
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-05-23 20:14:29 +0530

    moved the BroderControlPanel into a different as a separate component

--- frontend/src/Canvas/index.tsx
+++ frontend/src/Canvas/index.tsx
@@ -5,20 +5,6 @@ import { elementType } from "@/Types/canvas/elementsType";
 import { PublishFeature } from "./PublishFeature";
 
 import {
-    updateBoderRadius,
-    updateRightWidth,
-    updateLeftWidth,
-    updateBottomWidth,
-    updateTopWidth,
-    updateElementBoderWidth,
-    updateBottomLeftRadius,
-    updateBottomRightRadius,
-    updateTopRightRadius,
-    updateTopLeftRadius,
-    updateBorderStyle,
-} from "../store/toolbar/borderControl/borderControl";
-
-import {
     AlignCenter,
     AlignCenterHorizontal,
     AlignCenterVertical,
@@ -34,16 +20,10 @@ import {
     ArrowUp,
     Bold,
     Italic,
-    Minus,
     Monitor,
     RotateCcw,
-    Scan,
     Settings,
     Smartphone,
-    Square,
-    SquareDashed,
-    SquareDashedTopSolid,
-    SquareRoundCorner,
     Tablet,
     Type,
     Underline,
@@ -58,9 +38,9 @@ import {
     udpateScreenSizeStatus,
     updateScreenSize,
 } from "@/api/screenSizes";
-import { useDispatch, useSelector } from "react-redux";
 import { StoreState } from "@/store/store";
-import { increment } from "@/store/counter/counterSlice";
+import { BorderControlPanel } from "./ToolBar/BroderControlPanel";
+import { useSelector } from "react-redux";
 
 type CanvasElement = {
     id: number;
@@ -105,6 +85,7 @@ export const Canvas: React.FC = () => {
 
         borderStyle,
     } = useSelector((state: StoreState) => state.borderControl);
+
     const [toggleAllSide_radius, setToggleAllSide_radius] = useState<
         "all" | "specific"
     >("all");
@@ -419,316 +400,7 @@ export const Canvas: React.FC = () => {
         </div>
     );
 };
-
-export const BorderControlPanel = ({
-    toggleAllSide_radius,
-    toggleAllSide_width,
-
-    setToggleAllSide_radius,
-    setToggleAllSide_width,
-}: any) => {
-    const borderIconSize = 16;
-
-    const {
-        elementRadius,
-
-        topLeftRadius,
-        topRightRadius,
-        bottomRightRadius,
-        bottomLeftRadius,
-
-        elementBoderWidth,
-
-        topWidth,
-        bottomWidth,
-        leftWidth,
-        rightWidth,
-
-        borderStyle,
-    } = useSelector((state: StoreState) => state.borderControl);
-    const dispatch = useDispatch();
-
-    return (
-        <div className="border-section-container toolbar-section">
-            <div className="heading">border</div>
-            <div className="border-tools-container">
-                <div className="border-width-sub-section">
-                    <div className="sub-heading">width</div>
-                    <div className="border-width-tools-container">
-                        <div className="boder-width-adjustments-container">
-                            <div className="boder-width-adjustments">
-                                <div className="border-width border-tool">
-                                    <div className="border-width-icon">
-                                        <Minus size={borderIconSize} />
-                                    </div>
-                                    <input
-                                        type="number"
-                                        value={elementBoderWidth}
-                                        onChange={(e) =>
-                                            dispatch(
-                                                updateElementBoderWidth(
-                                                    Number(e.target.value),
-                                                ),
-                                            )
-                                        }
-                                    />
-                                </div>
-                            </div>
-                            <div className="boder-width-sides-toggle">
-                                <div
-                                    className={`all-sides ${toggleAllSide_width == "all" ? "active" : ""}`}
-                                    onClick={() =>
-                                        setToggleAllSide_width("all")
-                                    }
-                                >
-                                    <Square size={borderIconSize} />
-                                </div>
-                                <div
-                                    className={`target-sides ${toggleAllSide_width == "specific" ? "active" : ""}`}
-                                    onClick={() =>
-                                        setToggleAllSide_width("specific")
-                                    }
-                                >
-                                    <SquareDashed size={borderIconSize} />
-                                </div>
-                            </div>
-                        </div>
-                        {toggleAllSide_width == "specific" && (
-                            <div className="boder-width-specific-sides-selection">
-                                <div className="top-side-wrapper border-sides border-tool">
-                                    <div className="top-border-wraper">
-                                        <SquareDashedTopSolid
-                                            className="top-border"
-                                            size={borderIconSize}
-                                        />
-                                    </div>
-                                    <input
-                                        type="number"
-                                        value={topWidth}
-                                        onChange={(e) =>
-                                            dispatch(
-                                                updateTopWidth(
-                                                    Number(e.target.value),
-                                                ),
-                                            )
-                                        }
-                                    />
-                                </div>
-                                <div className="bottom-side-wrapper border-sides border-tool">
-                                    <div className="bottom-border-wraper">
-                                        <SquareDashedTopSolid
-                                            className="bottom-border"
-                                            size={borderIconSize}
-                                        />
-                                    </div>
-                                    <input
-                                        type="number"
-                                        value={bottomWidth}
-                                        onChange={(e) =>
-                                            dispatch(
-                                                updateBottomWidth(
-                                                    Number(e.target.value),
-                                                ),
-                                            )
-                                        }
-                                    />
-                                </div>
-                                <div className="left-side-wrapper border-sides border-tool">
-                                    <div className="left-border-wraper">
-                                        <SquareDashedTopSolid
-                                            className="left-border"
-                                            size={borderIconSize}
-                                        />
-                                    </div>
-                                    <input
-                                        type="number"
-                                        value={leftWidth}
-                                        onChange={(e) =>
-                                            dispatch(
-                                                updateLeftWidth(
-                                                    Number(e.target.value),
-                                                ),
-                                            )
-                                        }
-                                    />
-                                </div>
-                                <div className="right-side-wrapper border-sides border-tool">
-                                    <div className="right-border-wraper">
-                                        <SquareDashedTopSolid
-                                            className="right-border"
-                                            size={borderIconSize}
-                                        />
-                                    </div>
-                                    <input
-                                        type="number"
-                                        value={rightWidth}
-                                        onChange={(e) =>
-                                            dispatch(
-                                                updateRightWidth(
-                                                    Number(e.target.value),
-                                                ),
-                                            )
-                                        }
-                                    />
-                                </div>
-                            </div>
-                        )}
-                    </div>
-                </div>
-
-                <div className="border-width-sub-section">
-                    <div className="sub-heading">Style</div>
-                    <div className="border-width-tools-container">
-                        <select
-                            className="select-drop-down"
-                            value={borderStyle}
-                            onChange={(e) =>
-                                dispatch(updateBorderStyle(e.target.value))
-                            }
-                        >
-                            <option value={"solid"}>solid</option>
-                            <option value={"dotted"}>dotted</option>
-                            <option value={"dashed"}>dashed</option>
-                            <option value={"double"}>double</option>
-                            <option value={"groove"}>groove</option>
-                            <option value={"ridge"}>ridge</option>
-                            <option value={"inset"}>inset</option>
-                            <option value={"outset"}>outset</option>
-                            <option value={"none"}>none</option>
-                            <option value={"hidden"}>hidden</option>
-                        </select>
-                    </div>
-                </div>
-
-                <div className="border-radius-sub-section">
-                    <div className="sub-heading">radius</div>
-                    <div className="border-radius-tools-container">
-                        <div className="boder-radius-adjustments-container">
-                            <div className="boder-radius-adjustments">
-                                <div className="border-radius border-tool">
-                                    <div className="border-radius-icon">
-                                        <SquareRoundCorner
-                                            size={borderIconSize}
-                                        />
-                                    </div>
-                                    <input
-                                        type="number"
-                                        onChange={(e) =>
-                                            dispatch(
-                                                updateBoderRadius(
-                                                    Number(e.target.value),
-                                                ),
-                                            )
-                                        }
-                                        value={elementRadius}
-                                    />
-                                </div>
-                            </div>
-                            <div className="boder-radius-sides-toggle">
-                                <div
-                                    className={`all-sides ${toggleAllSide_radius == "all" ? "active" : ""}`}
-                                    onClick={() =>
-                                        setToggleAllSide_radius("all")
-                                    }
-                                >
-                                    <Square size={borderIconSize} />
-                                </div>
-                                <div
-                                    className={`target-sides ${toggleAllSide_radius == "specific" ? "active" : ""}`}
-                                    onClick={() =>
-                                        setToggleAllSide_radius("specific")
-                                    }
-                                >
-                                    <Scan size={borderIconSize} />
-                                </div>
-                            </div>
-                        </div>
-                        {toggleAllSide_radius == "specific" && (
-                            <div className="boder-radius-specific-sides-selection">
-                                <div className="top-side-wrapper border-sides border-tool">
-                                    <div className="top-border-wraper">
-                                        <SquareRoundCorner
-                                            className="top-border"
-                                            size={borderIconSize}
-                                        />
-                                    </div>
-                                    <input
-                                        type="number"
-                                        value={topLeftRadius}
-                                        onChange={(e) =>
-                                            dispatch(
-                                                updateTopLeftRadius(
-                                                    Number(e.target.value),
-                                                ),
-                                            )
-                                        }
-                                    />
-                                </div>
-                                <div className="bottom-side-wrapper border-sides border-tool">
-                                    <div className="bottom-border-wraper">
-                                        <SquareRoundCorner
-                                            className="bottom-border"
-                                            size={borderIconSize}
-                                        />
-                                    </div>
-                                    <input
-                                        type="number"
-                                        value={topRightRadius}
-                                        onChange={(e) =>
-                                            dispatch(
-                                                updateTopRightRadius(
-                                                    Number(e.target.value),
-                                                ),
-                                            )
-                                        }
-                                    />
-                                </div>
-                                <div className="left-side-wrapper border-sides border-tool">
-                                    <div className="left-border-wraper">
-                                        <SquareRoundCorner
-                                            className="left-border"
-                                            size={borderIconSize}
-                                        />
-                                    </div>
-                                    <input
-                                        type="number"
-                                        value={bottomLeftRadius}
-                                        onChange={(e) =>
-                                            dispatch(
-                                                updateBottomLeftRadius(
-                                                    Number(e.target.value),
-                                                ),
-                                            )
-                                        }
-                                    />
-                                </div>
-                                <div className="right-side-wrapper border-sides border-tool">
-                                    <div className="right-border-wraper">
-                                        <SquareRoundCorner
-                                            className="right-border"
-                                            size={borderIconSize}
-                                        />
-                                    </div>
-                                    <input
-                                        type="number"
-                                        value={bottomRightRadius}
-                                        onChange={(e) =>
-                                            dispatch(
-                                                updateBottomRightRadius(
-                                                    Number(e.target.value),
-                                                ),
-                                            )
-                                        }
-                                    />
-                                </div>
-                            </div>
-                        )}
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-};
+//
 
 export const FontsControlPanel = () => {
     const [activeDecoration, setActiveDecoration] = useState("bold");
