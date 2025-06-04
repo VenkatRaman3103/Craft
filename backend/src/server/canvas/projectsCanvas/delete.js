5435fee1d32ca3f8a51319c593745a40f5222595
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-06-04 11:43:33 +0530

    feat(projectCanvasRouter): added a `DELETE /projects-canvas/:id` route to delete `projectsCanvas` by `id`

---
+++ backend/src/server/canvas/projectsCanvas/delete.js
@@ -0,0 +1,22 @@
+import { eq } from "drizzle-orm";
+import { projectsCanvas } from "../../../db/schema/canvas/projectCanvas/schema.js";
+import { db } from "../../server.js";
+
+export const deleteAllProjectsById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const response = await db
+            .delete(projectsCanvas)
+            .where(eq(projectsCanvas.project_id, id))
+            .returning();
+        res.json(response[0]);
+    } catch (error) {
+        const errorMessage = {
+            error,
+            message: `Error in creating the block`,
+            origin: "backend/deleteAllProjectsById/DELETE",
+        };
+        console.log(errorMessage);
+        res.status(500).json(errorMessage);
+    }
+};
--- backend/src/server/canvas/projectsCanvas/route.js
+++ backend/src/server/canvas/projectsCanvas/route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { createProjects } from "./create.js";
 import { getAllProjects, getAllProjectsById } from "./read.js";
+import { deleteAllProjectsById } from "./delete.js";
 
 export const projectCanvasRouter = express.Router();
 
@@ -10,3 +11,6 @@ projectCanvasRouter.post("/projects-canvas", createProjects);
 // read
 projectCanvasRouter.get("/projects-canvas", getAllProjects); // all
 projectCanvasRouter.get("/projects-canvas/:id", getAllProjectsById); // id
+
+// delete
+projectCanvasRouter.delete("/projects-canvas/:id", deleteAllProjectsById); // id
