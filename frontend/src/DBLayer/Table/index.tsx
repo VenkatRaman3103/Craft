adf7fbb06697ef2d2d65dac6ad85b309ee935061
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-06-27 15:42:30 +0530

    feat(Table): connected the api with the Table

--- frontend/src/DBLayer/Table/index.tsx
+++ frontend/src/DBLayer/Table/index.tsx
@@ -1,6 +1,16 @@
 import { getAllCollections } from "@/api/DBLayer/getAllCollections";
 import { useQuery } from "@tanstack/react-query";
 
+export type CollectionType = {
+    collection_id: string;
+    createdAt: string;
+    name: string;
+    reference_id: string | null;
+    slug: string;
+    status: string;
+    type: string | null;
+};
+
 export const Table = () => {
     const { data } = useQuery({
         queryFn: () => getAllCollections(),
@@ -11,14 +21,29 @@ export const Table = () => {
 
     return (
         <div>
-            <Row />
+            <table>
+                <thead>
+                    {Object.keys(data[0]).map((item, ind) => (
+                        <td key={ind}>{item}</td>
+                    ))}
+                </thead>
+                <tbody>
+                    {data.map((row: CollectionType) => (
+                        <tr key={row.collection_id}>
+                            {Object.values(row).map((column, ind) => (
+                                <td key={ind}>{column}</td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 };
 
 export const Row = () => {
     return (
-        <div>
+        <div className="row-container">
             <div>Row</div>
         </div>
     );
