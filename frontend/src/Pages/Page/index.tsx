102cb8ef464bc628e911ec86f5346fef2286d969
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-07-22 06:28:01 +0530

    update

--- frontend/src/Pages/Page/index.tsx
+++ frontend/src/Pages/Page/index.tsx
@@ -58,6 +58,7 @@ export const Page = () => {
         }
 
         if (parentCollectionId) {
+            //
             getLocalFields();
         }
     }, [pageData, parentCollectionId]);
--- frontend/src/Paper/index.tsx
+++ frontend/src/Paper/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 import "./index.scss";
+
 export const Paper = () => {
     return (
         <div>
