82cfdca861e6d3f24a62c551af322ee059fd741d
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-08-23 12:03:32 +0530

    update

--- frontend/src/components/CollectionPreview/index.tsx
+++ frontend/src/components/CollectionPreview/index.tsx
@@ -11,7 +11,10 @@ export const CollectionPreview = ({ collection }: collectionType) => {
     const { name, slug } = collection;
 
     return (
-        <div className="collection-preview">
+        <div
+            className="collection-preview"
+            onClick={() => window.open(`/collections/${slug}`, "_blank")}
+        >
             <h2 className="collection-preview-heading">{name}</h2>
             <p className="collection-preview-slug">
                 slug: <span>{slug}</span>
