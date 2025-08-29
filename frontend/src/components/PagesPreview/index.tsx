7986a24cc4c274c4fb3bf988c7ee6792ce1a81c2
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-08-30 00:34:52 +0530

    feat(PagesPreview): added preview component for pages and connected it with api data

--- frontend/src/components/PagesPreview/index.tsx
+++ frontend/src/components/PagesPreview/index.tsx
@@ -1,9 +1,22 @@
+import { SlugLabel } from "../ui/SlugLabel";
 import "./index.scss";
 
 export const PagesPreview = ({ page }: any) => {
     return (
-        <div>
-            <div>{page.name}</div>
+        <div className="page-preview-container">
+            <div className="page-preview-wrapper">
+                <div className="page-preview-header">
+                    <div className="page-heading">
+                        <div className="page-icon"></div>
+                        {page.name}
+                    </div>
+                    <SlugLabel label={page.slug} />
+                </div>
+
+                <div className="page-preview-description">
+                    <p className="page-description">{page.description}</p>
+                </div>
+            </div>
         </div>
     );
 };
