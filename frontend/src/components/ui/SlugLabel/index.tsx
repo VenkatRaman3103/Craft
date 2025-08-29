e492be1b98e0c3227df0e8128997b9aafc7eead2
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-08-30 00:52:03 +0530

    UI tweaks

--- frontend/src/components/ui/SlugLabel/index.tsx
+++ frontend/src/components/ui/SlugLabel/index.tsx
@@ -1,9 +1,15 @@
+import { Copy } from "lucide-react";
+
 import "./index.scss";
 
 export const SlugLabel = ({ label }: { label: string }) => {
     return (
         <p className="slug-label">
-            slug: <span>{label}</span>
+            slug:{" "}
+            <span>
+                {label}
+                <Copy size={13} strokeWidth={2.25} className="copy-icon" />
+            </span>
         </p>
     );
 };
