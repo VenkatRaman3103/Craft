c5e0dfa807f452348a339697fe1c2786cc95a61c
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-09-07 10:25:04 +0530

    update

--- frontend/src/components/CollectionPreview/index.stories.tsx
+++ frontend/src/components/CollectionPreview/index.stories.tsx
@@ -1,7 +1,7 @@
 import { ComponentProps } from "react";
 import { CollectionPreview } from ".";
 import { Meta, StoryObj } from "@storybook/react";
-// bar
+// foo
 
 type storyPropType = ComponentProps<typeof CollectionPreview>;
 
