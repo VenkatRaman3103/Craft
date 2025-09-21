d176051e477b8fc3d347775d81bc9c4d9ad564f2
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-09-21 10:48:11 +0530

    feat(groups): added `PUT /groups/:id` to update the groups

--- backend/src/server/Groups/route.js
+++ backend/src/server/Groups/route.js
@@ -77,4 +77,28 @@ groupsRouter.get("/groups/:id", async (req, res) => {
 });
 
 // update
+groupsRouter.put("/groups/:id", async (req, res) => {
+    const { id } = req.params;
+    const { title, description } = req.body;
+
+    try {
+        const response = await db
+            .update(groups)
+            .set({ title, description })
+            .where(eq(groups.id, id))
+            .returning();
+
+        res.json(response[0]);
+    } catch (error) {
+        const errorMessage = {
+            origin: "groupsRouter/GET -> /groups/:id",
+            error: error,
+        };
+
+        console.log(errorMessage);
+
+        res.json(errorMessage);
+    }
+});
+
 // delete
