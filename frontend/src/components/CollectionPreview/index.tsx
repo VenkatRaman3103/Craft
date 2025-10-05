a277e95d5f9b6e2b6f4fb2559b11835de6375e0a
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-10-05 21:40:06 +0530

    update

--- frontend/src/components/CollectionPreview/index.tsx
+++ frontend/src/components/CollectionPreview/index.tsx
@@ -4,8 +4,10 @@ export const CollectionPreview = ({ data }: any) => {
     return (
         <div className="collection-preview-container">
             <div className="collection-preview-wrapper">
-                <div>{data.name}</div>
-                <div>{data.description}</div>
+                <div className="collection-name">{data.name}</div>
+                <div className="collection-slug">
+                    slug: <span>{data.slug}</span>
+                </div>
                 <div className="new-collection-button-wrapper">
                     <div>+</div>
                 </div>
