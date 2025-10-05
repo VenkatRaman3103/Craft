ee3e894e1d17cdaf91a08abf4d940e50154fe81a
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-10-06 00:58:10 +0530

    feat(NewCollectionModal): added fields

--- frontend/src/components/Modals/NewCollectionModal/index.tsx
+++ frontend/src/components/Modals/NewCollectionModal/index.tsx
@@ -1,9 +1,82 @@
+import React, { useEffect } from "react";
 import "./index.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/store";
+import { toggleModal } from "@/store/ModalSlice";
 
 export const NewCollectionModal = () => {
+    const { active: isModalActive } = useSelector(
+        (state: RootState) => state.modalSlice,
+    );
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const originalStyle = window.getComputedStyle(document.body).overflow;
+        document.body.style.overflow = "hidden";
+
+        return () => {
+            document.body.style.overflow = originalStyle;
+        };
+    }, []);
+
+    function handleClickOutside() {
+        dispatch(toggleModal(false));
+    }
+
     return (
-        <div className="new-collection-modal-container">
-            <div>NewCollection</div>
+        <div
+            className="new-collection-modal-overlay"
+            onClick={handleClickOutside}
+        >
+            <div
+                className="new-collection-modal-container"
+                onClick={(e) => e.stopPropagation()}
+            >
+                <div className="modal-header modal-section">
+                    <div>Add new Collection</div>
+                </div>
+
+                <div className="modal-section">
+                    <label>Name</label>
+                    <input type="text" placeholder="Collection Name" />
+                    <div className="description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Officia animi, autem expedita dolorem laudantium, aut
+                        tenetur!
+                    </div>
+                </div>
+
+                <div className="modal-section">
+                    <label>Slug</label>
+                    <input type="text" placeholder="Collection Slug" />
+                    <div className="description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Officia animi, autem expedita dolorem laudantium, aut
+                        tenetur!
+                    </div>
+                </div>
+
+                <div className="modal-section">
+                    <label>Description</label>
+                    <input
+                        type="textarea"
+                        placeholder="Collection Description"
+                    />
+                    <div className="description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Officia animi, autem expedita dolorem laudantium, aut
+                        tenetur!
+                    </div>
+                </div>
+
+                <div className="modal-action-button-wrapper">
+                    <div className="action-button" onClick={handleClickOutside}>
+                        Cancel
+                    </div>
+                    <div className="action-button save">Save</div>
+                </div>
+            </div>
         </div>
     );
 };
--- frontend/src/components/Modals/RenderModal.tsx
+++ frontend/src/components/Modals/RenderModal.tsx
@@ -1,4 +1,4 @@
-import { NewCollectionModal } from "./NewCollection";
+import { NewCollectionModal } from "./NewCollectionModal";
 
 export const RenderModal = ({ type }: { type: string | null }) => {
     function modelaRender(type: string | null) {
