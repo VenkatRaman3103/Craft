23ef33de395d66bd28bc45a29566ab4d951b8db3
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-10-23 22:13:42 +0530

    feat(NewCollection): after creating new collection the modal will be closed

--- frontend/src/mutations/collections/create.tsx
+++ frontend/src/mutations/collections/create.tsx
@@ -1,15 +1,19 @@
 import { createNewCollectionUnderGroups } from "@/api/createNewCollection/underGroups";
+import { toggleModal } from "@/store/ModalSlice";
 import { NewCollectionType } from "@/type/NewCollection";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useDispatch } from "react-redux";
 
 export function useCreateCollection() {
     const queryClient = useQueryClient();
+    const dispatch = useDispatch();
 
     return useMutation({
         mutationFn: (obj: NewCollectionType) =>
             createNewCollectionUnderGroups(obj),
         onSuccess: () => {
             queryClient.invalidateQueries({ queryKey: ["groups"] });
+            dispatch(toggleModal(false));
         },
     });
 }
