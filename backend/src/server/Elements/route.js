e02e114d47452abb6da468854ee5917f3ac0770d
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-10-26 20:58:33 +0530

    feat(Elements): added a route `/elements` to read all the elements

--- backend/src/server/Elements/route.js
+++ backend/src/server/Elements/route.js
@@ -1,7 +1,24 @@
 import express from "express";
+import { elements } from "../../db/schema/Elements/schema.js";
+import { db } from "../server.js";
 
 export const ElementsRouter = express.Router();
 
-ElementsRouter.get("/elements", async (req, res) => {
-    res.json("hello world");
+// read
+// all elements
+ElementsRouter.get("/elements", async (_, res) => {
+    try {
+        const response = await db.select().from(elements);
+
+        res.json(response);
+    } catch (error) {
+        const errorMessage = {
+            origin: "elements/GET -> /elements",
+            error: error,
+        };
+
+        console.log(errorMessage);
+
+        res.json(errorMessage);
+    }
 });
