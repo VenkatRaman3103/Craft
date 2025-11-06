fd2d5a14a34768a39d0d85eb0ef9b04beea7f2d6
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-11-06 12:18:49 +0530

    feat(StructuredContent): added route `/structured-content` to get structured data

--- backend/src/server/StructuredContent/route.js
+++ backend/src/server/StructuredContent/route.js
@@ -1,7 +1,55 @@
 import express from "express";
+import { db } from "../server.js";
+import { groups } from "../../db/schema/Groups/schema.js";
+import { collections } from "../../db/schema/Collections/schema.js";
+import { eq } from "drizzle-orm";
+import { elements } from "../../db/schema/Elements/schema.js";
+import { pages } from "../../db/schema/Pages/schema.js";
 
 export const StructuredContentRouter = express.Router();
 
-StructuredContentRouter.get("/structured-content", (req, res) => {
-    res.json("hello StructuredContentRouter");
+StructuredContentRouter.get("/structured-content", async (req, res) => {
+    try {
+        let result = [];
+
+        const allGroups = await db.select().from(groups);
+
+        for (let group of allGroups) {
+            let colResult = [];
+
+            const childCollections = await db
+                .select()
+                .from(collections)
+                .where(eq(collections.group_id, group.id));
+
+            for (let col of childCollections) {
+                let elementList = [];
+
+                const eleRes = await db
+                    .select()
+                    .from(elements)
+                    .where(eq(elements.parent_col_id, col.id));
+
+                for (let ele of eleRes) {
+                    const pageList = await db
+                        .select()
+                        .from(pages)
+                        .where(eq(pages.parent_element_id, ele.id));
+
+                    elementList.push({ ...ele, pages: pageList });
+                }
+
+                colResult.push({ ...col, elements: elementList });
+            }
+
+            result.push({ ...group, collections: colResult });
+        }
+
+        res.json(result);
+    } catch (error) {
+        res.json({
+            origin: "structured-content/GET -> /structured-content",
+            error,
+        });
+    }
 });
