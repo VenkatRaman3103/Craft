40e2a314324d7c16cc483e5cb87729058e1caf82
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-11-20 22:16:42 +0530

    feat(PagesVersion): added a route "/pages-version/:page_id" to get page's version by its id

--- backend/src/server/Pages/PagesVersion/route.js
+++ backend/src/server/Pages/PagesVersion/route.js
@@ -1,15 +1,36 @@
 import express from "express";
 import { pages_versions } from "../../../db/schema/Pages/PagesVersion/schema.js";
 import { db } from "../../server.js";
+import { eq } from "drizzle-orm";
 
 export const PagesVersionRouter = express.Router();
 
 // get version of the page by its id
+PagesVersionRouter.get("/pages-version/:page_id", async (req, res) => {
+    const { page_id } = req.params;
+
+    try {
+        const reponse = await db
+            .select()
+            .from(pages_versions)
+            .where(eq(pages_versions.page_id, page_id));
+        res.json(reponse);
+    } catch (error) {
+        const errorMessage = {
+            origin: "pages_versions/GET -> /pages-version/:page_id",
+            error: error,
+        };
+
+        console.log(errorMessage);
+
+        res.json(errorMessage);
+    }
+});
+
 // create a version with the page data: page data -> content_json
 PagesVersionRouter.post("/pages-version/:page_id", async (req, res) => {
     const { page_id } = req.params;
     const { version_number, page_data, published_at, created_by } = req.body;
-    console.log(req.body, "<- body");
 
     try {
         const reponse = await db
