3f72e369eca8ee4698f666abece5a577be757272
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-11-29 07:38:25 +0000

    feat(TextField): added create route TextField

--- backend/src/server/Fields/TextField/route.js
+++ backend/src/server/Fields/TextField/route.js
@@ -1,7 +1,29 @@
 import express from "express";
+import { textFields } from "../../../db/schema/Fields/TextFields.js";
+import { db } from "../../server.js";
 
 export const TextFieldRouter = express.Router();
 
-TextFieldRouter.get("/text-field", async (req, res) => {
-    res.json("hello from text-field");
+TextFieldRouter.post("/text-field/:section_id", async (req, res) => {
+    const { section_id } = req.params;
+
+    const { name, value } = req.body;
+
+    try {
+        const response = await db
+            .insert(textFields)
+            .values({ name, value, section_id })
+            .returning();
+
+        res.json(response);
+    } catch (error) {
+        const errorMessage = {
+            origin: "TextFieldRouter/GET -> /text-field",
+            error: error,
+        };
+
+        console.log(errorMessage);
+
+        res.json(errorMessage);
+    }
 });
