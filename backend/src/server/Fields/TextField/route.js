dae8e3ea793c19a990fba4b963ec0e9c2c0e47a6
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-11-29 07:46:25 +0000

    feat(TextField): added read route for TextField

--- backend/src/server/Fields/TextField/route.js
+++ backend/src/server/Fields/TextField/route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { textFields } from "../../../db/schema/Fields/TextFields.js";
 import { db } from "../../server.js";
+import { eq } from "drizzle-orm";
 
 export const TextFieldRouter = express.Router();
 
@@ -19,6 +20,28 @@ TextFieldRouter.post("/text-field/:section_id", async (req, res) => {
     } catch (error) {
         const errorMessage = {
             origin: "TextFieldRouter/POST -> /text-field",
+            error: error,
+        };
+
+        console.log(errorMessage);
+
+        res.json(errorMessage);
+    }
+});
+
+TextFieldRouter.get("/text-field/:id", async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const response = await db
+            .select()
+            .from(textFields)
+            .where(eq(textFields.id, id));
+
+        res.json(response);
+    } catch (error) {
+        const errorMessage = {
+            origin: "TextFieldRouter/GET -> /text-field",
             error: error,
         };
 
