f16b7e389cf3ad050e43e57a7ea9d24705174fd8
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-11-30 18:26:17 +0000

    UI tweaks

--- frontend/src/components/ui/Buttons/SaveBtn/index.tsx
+++ frontend/src/components/ui/Buttons/SaveBtn/index.tsx
@@ -1,10 +1,21 @@
+import { useSelector } from "react-redux";
 import "./index.scss";
 import { ArrowBigUpDash, Plus, Save } from "lucide-react";
+import { RootState } from "@/store";
+import { useEffect, useState } from "react";
 
 export const SaveBtn = ({ onClickFn }: { onClickFn: any }) => {
+    const [active, setActive] = useState(false);
+
+    const { dataBucket } = useSelector((state: RootState) => state.itemsBucket);
+
+    useEffect(() => {
+        setActive(Object.keys(dataBucket).length > 0);
+    }, [dataBucket]);
+
     return (
         <button
-            className="btn with-icon btn-secondary  btn-lg"
+            className={`btn with-icon btn-secondary btn-lg save-btn ${active ? "active" : ""}`}
             onClick={() => onClickFn()}
         >
             <Save size={18} />
